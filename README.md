5a62459e896028ab85052997dbeeb6c86307db39
jbrockdev <jbrockwork33@osgsmail.icu>
2021-08-15 15:59:22 -0700

    Finalized function that takes in any questions and random response with regenerating responses when all are used.

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 ## About Me
 
-I build an app with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the app
+This was a project for the Code Fellows 'Code 201' course. Our goal was to make guestions with prompts and responses via alerts.
 
 ### Author: Jonathan Brock
 
 ### Links and Resources
 
 ### Reflections and Comments
-* Consider including the answers to your daily journal and submission questions here
-* This is also a good place to reflect on the tools and resources used and learned
+
+We started with yes or no questions and then moved onto a number guessing game. After the number guessing game, we introduced an array of correct answers. The final result was a function that will take in any question whether it's a yes or no question, number, array of numbers, or array of words as well as offering varied attempts regardless of the question.
+
+We also worked with CSS a little bit. The header is set to a fixed position to be at the top of the page even when scrolling. The sections for information below bio are floated to the left to be on the same line as one another. The bio section has width set to 66% of screen size and margin of 16.6% to be positioned in the middle 2/3 of the screen.
--- js/app.js
+++ js/app.js
@@ -12,17 +12,21 @@ let usersName = prompt("Please provide your name.");
 
 alert("Welcome to my About Me page, " + usersName + ". Now before I tell you about me, I'm going to have you guess a few facts about me! Let's begin.");
 
-let responsesArray = [ // Index 0 - right answers, Index 1 - wrong answers
+let responsesArrayOriginal = [ // Index 0 - right answers, Index 1 - wrong answers
   ["Correct, you rock!", "10-4, you are right!", "You were wrong. SIKE! Good job.", "I knew you were a good person!", "Are you psychic?", "Clearly we see eye to eye, hire me?", "Absolutely astounding!", "I think we could be friends!", "There are 10 reasons I like you; you are checking out my page and you got this right."],
   ["Are you an alien? WRONG!", "You meant to get this wrong, right?", "Abort mission, weirdo detected.", "I bet you think dogs are better than cats, wrong.", "Differences make a stronger team, hire me?", "You should be locked up for thinking that.", "I think this is my queue to leave.", "I could flip a penny and get more answers right than you.", "Statistically, getting this wrong increases the chances of getting the next right.", "We grow the most through failures."]
 ]
+let responsesArray = [];
+responsesArray[0] = [...responsesArrayOriginal[0]];
+responsesArray[1] = [...responsesArrayOriginal[1]];
+
 
 let questionArray = [
-//  ["Is my favorite color green?", true, 1],
-//  ["Am I a college graduate?", false, 1],
-//  ["Have I professionally played poker?", true, 1],
-//  ["Do I play single player video games?", false, 1],
-//  ["Do I live in California?", false, 1],
+  ["Is my favorite color green?", true, 1],
+  ["Am I a college graduate?", false, 1],
+  ["Have I professionally played poker?", true, 1],
+  ["Do I play single player video games?", false, 1],
+  ["Do I live in California?", false, 1],
   ["Guess a number 1-100.", getRandomNumber(1, 100), 4],
   ["Guess one of my favorite numbers.", [33, 19, 99], 6],
   ["Guess one of my favorite games.", ["league of legends", "new world", "warzone"], 6]
@@ -48,7 +52,6 @@ function numberGuessQuestion(currentAnswer, guessPrompt, currentAttempt, maxAtte
   let isCorrect = false;
   let numberGuess = parseInt(guessPrompt);
   console.log("For seeing correct answer path easier only: " + currentAnswer);
-
   if (currentAnswer === numberGuess) {
     isCorrect = true;
     alertMsg = "DING DING DING";
@@ -58,6 +61,9 @@ function numberGuessQuestion(currentAnswer, guessPrompt, currentAttempt, maxAtte
     alertMsg = "Your guess is too low.";
   } else {
     alertMsg = "Please clean up the drool from your keyboard. -1 attempt.";
+  }
+  if (isCorrect || currentAttempt === maxAttempts - 1) {
+    alertMsg += " The correct answer was " + currentAnswer;
   }
   alert(alertMsg);
   return isCorrect;
@@ -70,7 +76,6 @@ function arrayQuestion(currentAttempt, currentAnswer, guessPrompt, lowercaseGues
   if (typeof(currentAnswer[0]) === "number") {
     isNumberArray = true;
   }
-
   for (let i = 0; i < currentAnswer.length; i++) {
     if (isNumberArray) {
       if (currentAnswer[i] === parseInt(guessPrompt)) {
@@ -105,25 +110,37 @@ function handleAfterArrayQuestionResponse(isCorrect, currentAnswer, stringFromAr
   }
 }
 
+function getRandomResponse(isCorrect) {
+  let rnd;
+  let index;
+  if (isCorrect) {
+    index = 0;
+  } else {
+    index = 1;
+  }
+  rnd = getRandomNumber(0, responsesArray[index].length - 1);
+  let randomResponse = responsesArray[index][rnd];
+  responsesArray[index].splice(rnd, 1);
+  if (responsesArray[index].length === 0) {
+    responsesArray[index] = [...responsesArrayOriginal[index]];
+  }
+  return randomResponse;
+}
+
 function handleAfterQuestionResponse(isCorrect, questionNumber) {
   let randomResponse;
-  let rnd;
   if (isCorrect) {
     correctStreak++;
     incorrectStreak = 0;
     totalRight++;
-    rnd = getRandomNumber(0, responsesArray[0].length - 1);
-    randomResponse = responsesArray[0][rnd];
+    let randomResponse = getRandomResponse(isCorrect);
     alertMsg = randomResponse + "\r\nCorrect Streak: " + correctStreak;
   } else {
     incorrectStreak++;
     correctStreak = 0;
-    rnd = getRandomNumber(0, responsesArray[1].length - 1);
-    randomResponse = responsesArray[1][rnd];
+    let randomResponse = getRandomResponse(isCorrect);
     alertMsg = randomResponse + "\r\nIncorrect Streak: " + incorrectStreak;
   }
- 
-
   alertMsg += "\r\nCurrent Score: " + totalRight + " out of " + (questionNumber + 1);
   alert(alertMsg);
 }
@@ -133,11 +150,9 @@ function askQuestion(questionNumber) {
   let currentQuestion = currentQuestionArray[0];
   let currentAnswer = currentQuestionArray[1];
   let maxAttempts = currentQuestionArray[2];
-
   let currentAttempt = 0;
   let stringFromArray = "";
   let isCorrect = false;
-
   while (isCorrect === false && currentAttempt < maxAttempts) {
     let guessPrompt; 
     let promptMsg;
@@ -146,7 +161,6 @@ function askQuestion(questionNumber) {
       guessPrompt = prompt(promptMsg);
     }
     let lowercaseGuess = guessPrompt.toLowerCase();
-
     if (typeof(currentAnswer) === "boolean") { // this is for yes/no questions
       isCorrect = yesOrNoQuestion(currentAnswer, lowercaseGuess);
     } else if (typeof(currentAnswer) === "number") { // this is for a number guessing game
@@ -165,7 +179,7 @@ function askQuestion(questionNumber) {
 
 
 for (let i = 0; i < questionArray.length; i++) {
-  askQuestion(i);
+    askQuestion(i);
 }
 
 alertMsg = "Thanks for playing the guessing game, " + usersName + ".\r\nYou scored " + totalRight + " out of " + questionArray.length + ".\r\nYou may now view my page. PS. Hire me."
